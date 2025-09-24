a88ced08bf7bdae55ac88913c58c7902b5e9844b
Shane <shane.prent.sp@gmail.com>
2025-09-24 21:20:50 +0200

    Merged initial commit with github repo init

--- obj/Debug/net8.0/MatchmakingService.AssemblyInfo.cs
+++ obj/Debug/net8.0/MatchmakingService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MatchmakingService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fc52fd3a4e33766dad2fb83c4f14ffa50c7f3f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MatchmakingService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MatchmakingService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
