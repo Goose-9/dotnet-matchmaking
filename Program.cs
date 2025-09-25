5e50b8c774acca2622b9667ac5af72369d24510e
Shane <shane.prent.sp@gmail.com>
2025-09-25 21:26:06 +0200

    fix(program): fixed startup logic and removed unnecessary comments

--- Program.cs
+++ Program.cs
@@ -2,16 +2,12 @@ using Matchmaking.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Strategy registration: swap here later for Latency/Elo strategies
+// Strategy + engine
 builder.Services.AddSingleton<IMatchmakingStrategy, FifoQueueStrategy>();
-
-// Engine is a hosted background service
 builder.Services.AddSingleton<MatchmakingEngine>();
 builder.Services.AddHostedService(sp => sp.GetRequiredService<MatchmakingEngine>());
 
