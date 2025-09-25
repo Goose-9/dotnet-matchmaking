42f44446047a0eb8372a6c182e683ddce73c5ffb
Shane <shane.prent.sp@gmail.com>
2025-09-25 17:36:00 +0200

    chore: updated program.cs to connect strategies with engine and swagger.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
+using Matchmaking.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddControllers(); // <- Enables MVC controller support
+// Strategy registration: swap here later for Latency/Elo strategies
+builder.Services.AddSingleton<IMatchmakingStrategy, FifoQueueStrategy>();
+
+// Engine is a hosted background service
+builder.Services.AddSingleton<MatchmakingEngine>();
+builder.Services.AddHostedService(sp => sp.GetRequiredService<MatchmakingEngine>());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,7 +24,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-app.MapControllers(); // <- Maps routes from controller classes
+app.MapControllers(); 
 
 app.Run();
